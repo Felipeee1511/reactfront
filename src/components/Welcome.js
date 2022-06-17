e31e2b355a9c3b76d10a3e434ab435ee5c04ed54
Felipeee1511 <79226687+Felipeee1511@users.noreply.github.com>
2022-06-17 07:51:27 -0400

    Update welcome

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -2,7 +2,7 @@ export default function Welcome() {
 
     return(
         <div>
-            <h1> WENA WENAAAAAAAAAAAAA</h1>
+            <h1> Test programador inicial</h1>
         </div>
     )
 }
